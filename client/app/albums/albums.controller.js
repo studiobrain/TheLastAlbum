1b95fcd31c4784d73c3cfa396c81876afbd9e348
Josh Longbrake <jlongbrake@joshs-mac-mini.attlocal.net>
2015-02-18 19:02:25 -0600

    added animations and albums section with return to artist search

--- client/app/albums/albums.controller.js
+++ client/app/albums/albums.controller.js
@@ -10,6 +10,9 @@ angular.module('theLastAlbumApp')
       $scope.albumsResult = {};
       $scope.searchMessage = 'choose wisely';
       $scope.errorMessage = '';
+      $scope.searchBox = document.querySelector('.top');
+      $scope.artistSection = document.querySelector('.artists');
+      $scope.albumSection = document.querySelector('.albums');
 
       $scope.findArtist = function () {
         if ($scope.searchInput !== undefined) {
@@ -22,10 +25,11 @@ angular.module('theLastAlbumApp')
                 $scope.searchMessage = 'Umm...  Thats a new one to us!  Indie band maybe?';
                 return;
               } else {
+                
                 $scope.searchMessage = 'Found ' + $scope.artistsResult.length + ' artists matching: ' + $scope.currentArtist;
-                $scope.getThumbs();
+                $scope.getThumbs($scope.artistsResult);
               }
-              console.log('ROCK ON ARTISTS!:', $scope.artistsResult);
+              //console.log('ROCK ON ARTISTS!:', $scope.artistsResult);
             }, function (error) {
               console.log('Erroneous:', error);
             });
@@ -33,7 +37,34 @@ angular.module('theLastAlbumApp')
 
         }
       };
-
+      
+      $scope.slideButtons = function () {
+        if ($scope.searchBox.classList.contains('lifted')) {
+          $scope.searchBox.classList.remove('lifted');
+          $scope.searchBox.classList.add('dropped');
+          $scope.scrollSections(1);
+        } else {
+          $scope.searchBox.classList.remove('dropped');
+          $scope.searchBox.classList.add('lifted');
+          $scope.scrollSections(0);
+        }
+      }
+      
+      $scope.scrollSections = function scrollToPage(dir) {
+        var pagePos = 0;
+        if (dir === 0) {
+          pagePos = $scope.albumSection.offsetTop - 280;
+        } else {
+          pagePos = $scope.artistSection.offsetTop - 280;
+        }
+        TweenLite.to(window, 1, {
+          scrollTo: {
+            y: pagePos
+          },
+          ease: Circ.easeInOut,
+        });
+      }
+      
       $scope.getAlbums = function (result) {
         console.log('result', result.name);
         $scope.currentArtist = result.name;
@@ -41,25 +72,31 @@ angular.module('theLastAlbumApp')
           AlbumService.get($scope.currentArtist)
             .then(function (response) {
               $scope.albumsResult = response.data.album;
+              
+              //$scope.artistSection.classList.add('hidden');
+              //$scope.albumSection.classList.add('revealed');
               if ($scope.albumsResult === undefined) {
                 $window.alert('Bummer!  We cant find any albums based on that artist/combo...'); 
               }
-              console.log('ROCK ON ALBUMS!:', $scope.albumsResult);
+              //console.log('ROCK ON ALBUMS!:', $scope.albumsResult);
+              $scope.slideButtons();
+              $scope.getThumbs($scope.albumsResult);
+              $scope.searchMessage = 'Found ' + $scope.albumsResult.length + ' albums matching: ' + $scope.currentArtist;
             }, function (error) {
               console.log('Erroneous:', error);
             });
         }
       };
 
-      $scope.getThumbs = function () {
-        for (var i = 0; i < $scope.artistsResult.length; i++) {
-          var imageCheck = $scope.artistsResult[i].image[2][Object.keys($scope.artistsResult[i].image[2])[0]];
+      $scope.getThumbs = function (arr) {
+        for (var i = 0; i < arr.length; i++) {
+          var imageCheck = arr[i].image[2][Object.keys(arr[i].image[2])[0]];
           if (typeof imageCheck !== 'undefined' && imageCheck) {
-            console.log(imageCheck);
-            $scope.artistsResult[i].image = imageCheck;
+            //console.log(imageCheck);
+            arr[i].image = imageCheck;
           } else {
             //$scope.artistsResult.image = $scope.default;
-            console.log('default image');
+            //console.log('default image');
           }
         }
       };
